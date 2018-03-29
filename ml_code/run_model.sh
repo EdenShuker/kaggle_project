f7fc6723dc31c190442e7d2077f74285f19c51ce
Eden <edenshuker1997@gmail.com>
2018-03-29 13:23:34 +0300

    update project

--- ml_code/run_model.sh
+++ ml_code/run_model.sh
@@ -1,9 +1,6 @@
 
 # split train set to test and train
-if [ ! -d "data" ]; then
-    mkdir data
-fi
-python split_data.py data/train_labels_filtered.csv
+python ../split_data.py ../data/train_labels_filtered.csv
 
 # create features from test files
 if [ ! -d "ngrams" ]; then
@@ -11,7 +8,7 @@ if [ ! -d "ngrams" ]; then
 fi
 for i in {1..9}
 do
-    python extract_ngrams.py data/files -n $i -p data/train_set.csv
+    python extract_ngrams.py ../data/files -n $i -p ../data/train_set.csv
 done
 
 if [ ! -d "features" ]; then
@@ -26,9 +23,9 @@ python extract_segments.py
 if [ ! -d "f2v" ]; then
     mkdir f2v
 fi
-python f2v.py data/files data/train_set.csv f2v/train.f2v
-python f2v.py data/files data/test_set.csv f2v/test.f2v
+python f2v.py ../data/files ../data/train_set.csv f2v/train.f2v
+python f2v.py ../data/files ../data/test_set.csv f2v/test.f2v
 
 ## run model on splitted data
-python model.py  -train data/train_set.csv f2v/train.f2v -save first.model -test data/test_set.csv f2v/test.f2v test.output
-python eval_model.py data/test_set.csv test.output
+python model.py  -train ../data/train_set.csv f2v/train.f2v -save first.model -test ../data/test_set.csv f2v/test.f2v test.output
+python eval_model.py ../data/test_set.csv test.output
