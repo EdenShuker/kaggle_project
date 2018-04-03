fixed softmax using

deep_code/model.py
@@ -26,6 +26,7 @@ class MalConv(nn.Module):
         self.fc_2 = nn.Linear(128, 9)
 
         self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.Softmax()
 
         self.i2l = {i: l for i, l in enumerate(labels)}
 
@@ -44,7 +45,7 @@ class MalConv(nn.Module):
         x = self.fc_1(x)
         x = self.fc_2(x)
 
-        return nn.Softmax(x)
+        return self.softmax(x)
 
 
 def split_csv_dict(csv_filepath):

deep_code/utils.py
@@ -23,4 +23,4 @@ class ExeDataset(Dataset):
                     tmp.append(int(bytes_str, 16) + 1)
             tmp = tmp + [0] * (self.first_n_byte - len(tmp))
 
-        return np.array(tmp), np.array([self.label_list[idx]])
+        return np.array(tmp).astype('int32'), np.array([self.label_list[idx]]).astype('int32')